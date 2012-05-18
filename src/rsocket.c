7c8fbc95903cf8f61422d7f2c58f83ba4f0b8ac4
Sean Hefty <sean.hefty@intel.com>
2012-05-18 16:36:07 -0700

    rsockets: Define options specific to rsockets
    Allow a user to control some of the RDMA related attributes
    of an rsocket through setsockopt/getsockopt.  A user specifies
    that the rsocket should be modified through SOL_RDMA level.
    
    This patch provides the initial framework.  Subsequent patches
    will add the configurable parameters.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- src/rsocket.c
+++ src/rsocket.c
@@ -1661,6 +1661,8 @@ int rsetsockopt(int socket, int level, int optname,
 			break;
 		}
 		break;
+	case SOL_RDMA:
+		break;
 	default:
 		break;
 	}
@@ -1725,6 +1727,9 @@ int rgetsockopt(int socket, int level, int optname,
 			ret = ENOTSUP;
 			break;
 		}
+		break;
+	case SOL_RDMA:
+		ret = ENOTSUP;
 		break;
 	default:
 		ret = ENOTSUP;
