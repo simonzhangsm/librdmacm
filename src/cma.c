288090d29ae6d8e9f0a4095e0ede35fcf01073ae
Sean Hefty <sean.hefty@intel.com>
2012-07-18 09:01:43 -0700

    debug fork

--- src/cma.c
+++ src/cma.c
@@ -48,6 +48,7 @@
 #include <byteswap.h>
 #include <stddef.h>
 #include <netdb.h>
+#include <stdio.h>
 
 #include "cma.h"
 #include <infiniband/driver.h>
@@ -142,6 +143,7 @@ static void ucma_cleanup(void)
 		free(cma_dev_array);
 		cma_dev_cnt = 0;
 	}
+	printf("ucma_cleanup\n");
 }
 
 static int check_abi_version(void)
@@ -215,6 +217,7 @@ int ucma_init(void)
 	if (cma_dev_cnt)
 		return 0;
 
+	printf("ucma_init\n");
 	pthread_mutex_lock(&mut);
 	if (cma_dev_cnt) {
 		pthread_mutex_unlock(&mut);
