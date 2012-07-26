ae46429bed49504d5f3f89bb2e9b9840acca819b
Sean Hefty <sean.hefty@intel.com>
2012-07-26 15:35:32 -0700

    rsockets: Fix shutdown
    mpi finalize hangs - fix it
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- src/preload.c
+++ src/preload.c
@@ -801,6 +801,7 @@ int select(int nfds, fd_set *readfds, fd_set *writefds,
 int shutdown(int socket, int how)
 {
 	int fd;
+	fflush(stdout);
 	return (fd_get(socket, &fd) == fd_rsocket) ?
 		rshutdown(fd, how) : real.shutdown(fd, how);
 }
