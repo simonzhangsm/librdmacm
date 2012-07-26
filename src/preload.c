d155a24856d9636893ffc0bdd81a7d1e9fb094f7
Sean Hefty <sean.hefty@intel.com>
2012-07-26 15:37:14 -0700

    Refresh of mpi-finalize

--- src/preload.c
+++ src/preload.c
@@ -798,9 +798,12 @@ int select(int nfds, fd_set *readfds, fd_set *writefds,
 	return ret;
 }
 
+#include <stdio.h>
+
 int shutdown(int socket, int how)
 {
 	int fd;
+	printf("shutdown how %s\n", how == SHUT_RDWR ? "RDWR" : how == SHUT_RD ? "RD" : "WR");
 	return (fd_get(socket, &fd) == fd_rsocket) ?
 		rshutdown(fd, how) : real.shutdown(fd, how);
 }
