5f6035045a3c2dfa888ab2da865bcd16923a58a6
Sean Hefty <sean.hefty@intel.com>
2012-08-07 09:37:24 -0700

    rspreload: Add fstat support
    vsftpd calls fstat on a socket.  Fake it out.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- src/preload.c
+++ src/preload.c
@@ -86,7 +86,11 @@ struct socket_calls {
 			  void *optval, socklen_t *optlen);
 	int (*fcntl)(int socket, int cmd, ... /* arg */);
 	int (*dup2)(int oldfd, int newfd);
+<<<<<<< current
 	ssize_t (*sendfile)(int out_fd, int in_fd, off_t *offset, size_t count);
+=======
+	int (*fxstat64)(int ver, int fd, struct stat64 *buf);
+>>>>>>> patched
 };
 
 static struct socket_calls real;
@@ -279,7 +283,11 @@ static void init_preload(void)
 	real.getsockopt = dlsym(RTLD_NEXT, "getsockopt");
 	real.fcntl = dlsym(RTLD_NEXT, "fcntl");
 	real.dup2 = dlsym(RTLD_NEXT, "dup2");
+<<<<<<< current
 	real.sendfile = dlsym(RTLD_NEXT, "sendfile");
+=======
+	real.fxstat64 = dlsym(RTLD_NEXT, "__fxstat64");
+>>>>>>> patched
 
 	rs.socket = dlsym(RTLD_DEFAULT, "rsocket");
 	rs.bind = dlsym(RTLD_DEFAULT, "rbind");
@@ -1032,4 +1040,24 @@ ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count)
 		lseek(in_fd, ret, SEEK_CUR);
 	munmap(file_addr, count);
 	return ret;
+}
+
+int __fxstat64(int ver, int socket, struct stat64 *buf)
+{
+//	int fd, ret;
+
+//	if (fd_get(socket, &fd) == fd_rsocket) {
+//		ret = real.fxstat64(ver, socket, buf);
+//		if (!ret)
+//			buf->st_mode = (buf->st_mode & ~S_IFMT) | __S_IFSOCK;
+//	} else {
+//		ret = real.fxstat64(ver, fd, buf);
+//	}
+//	return ret;
+	return real.fxstat64(ver, socket, buf);
+}
+
+int __fxstat(int ver, int socket, struct stat *buf)
+{
+	return real.fxstat(ver, socket, buf);
 }
