d46647ae56c4018c5c0eac0cc3d4202073aeae16
Sean Hefty <sean.hefty@intel.com>
2012-08-20 09:06:49 -0700

    rspreload: Fix checks in fork_active
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- src/preload.c
+++ src/preload.c
@@ -520,7 +520,7 @@ static void fork_active(int socket)
 	if ((ret != sizeof msg) || msg)
 		goto err2;
 
-	ret = rconnect(ret, (struct sockaddr *) &addr, len);
+	ret = rconnect(dfd, (struct sockaddr *) &addr, len);
 	if (ret)
 		goto err2;
 
@@ -618,9 +618,9 @@ static inline enum fd_type fd_fork_get(int index, int *fd)
 
 	fdi = idm_lookup(&idm, index);
 	if (fdi) {
-		if (fdi->type == fd_fork_passive)
+		if (fdi->state == fd_fork_passive)
 			fork_passive(index);
-		else if (fdi->type == fd_fork_active)
+		else if (fdi->state == fd_fork_active)
 			fork_active(index);
 		*fd = fdi->fd;
 		return fdi->type;
